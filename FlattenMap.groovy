319373517a99d583d2d1effe73a8ab69c83a9033
Thornton <thornton@rosesquared.org>
2020-12-24 15:16:07 -0500

    Fix FlattenMap and add variation.

--- FlattenMap.groovy
+++ FlattenMap.groovy
@@ -1,13 +1,18 @@
-def flatten(Map src, Map dest, String prefix = "") {
+def flatten(Map src, Map dest=[:], String parentKey="") {
 	src.each { key, val ->
+		key = [parentKey, key].findAll().join(".")
+
 		if (val instanceof Map) {
-			flatten(src, dest, "$key.")
+			flatten(val, dest, key)
 		} else {
 			dest[key] = val
 		}
 	}
 
-	return dest
+	dest
 }
 
-println flatten([a: 1], [:])
+println flatten([a: 1])
+println flatten([a: 1, b: [c: 2]])
+println flatten([a: 1, b: [c: [d: 3]]])
+println flatten([a: 1, b: [c: [d: 2]], e: 3])
---
+++ FlattenMap2.groovy
@@ -0,0 +1,17 @@
+def flatten(Map src, Map dest=[:], String parentKey="") {
+	src.each { key, val -> flatten(val, dest, [parentKey, key].findAll().join(".")) }
+	// functional:
+	// src.each { key, val -> dest += flatten(val, dest, [parentKey, key].findAll().join(".")) }
+	dest
+}
+
+def flatten(val, Map dest, String key) {
+	dest[key] = val
+	// functional:
+	// dest + [(key): val]
+}
+
+println flatten([a: 1])
+println flatten([a: 1, b: [c: 2]])
+println flatten([a: 1, b: [c: [d: 3]]])
+println flatten([a: 1, b: [c: [d: 2]], e: 3])
